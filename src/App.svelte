e6ec7dcf70c82763cc3a878f77d7a17757e29ace
Timur Daudov <timur.daudov@namecheap.com>
2019-06-01 18:05:44 +0300

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+  import KittyApp from "./quests/kitty/Kitty.svelte";
+</script>
+
+<KittyApp />
---
+++ src/quests/kitty/Kitty.svelte
@@ -0,0 +1,10 @@
+<style>
+  .container {
+    margin: 0 auto;
+  }
+</style>
+
+<div class="container">
+  <h1>Show me the kitty</h1>
+</div>
+<hr />
